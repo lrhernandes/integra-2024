1099e5278ab026c8ab836eb15e8b5f97b866ba81
lrhernandes <larachernandes@gmail.com>
2024-02-19 23:09:15 -0300

    fix: routing

--- app/src/app/components/UsersTableWrapper.tsx
+++ app/src/app/components/UsersTableWrapper.tsx
@@ -12,14 +12,17 @@ const UsersTableWrapper: React.FC = () => {
     <>
       <div className="flex mb-8 mt-4 flex-row justify-between items-center w-full">
         <h2 className="text-2xl font-semibold">Users</h2>
-        <Button
-          text="ADD USER"
-          type="primary"
-          action={() => {
+        <button
+          onClick={() => {
             router.push("/users/new");
           }}
-          icon={FiPlus}
-        />
+          className="bg-purple-700 hover:bg-purple-900 text-white font-bold py-2 px-4 rounded flex flex-row justify-center items-center gap-2"
+        >
+          <span>
+            <FiPlus />
+          </span>
+          ADD USER
+        </button>
       </div>
       <Wrapper>
         <UsersTable />
